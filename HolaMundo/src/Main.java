9cb6f70f046b0055e6a505702972fc50ab2bebba
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-07-05 20:42:29 -0500

    Tipos de datos primitivos 'byte, short, int, long'

--- HolaMundo/src/Main.java
+++ HolaMundo/src/Main.java
@@ -9,9 +9,84 @@ public class Main {
         String s = "Despedirse";
         System.out.println(s);
  */
+/*
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese su nombre:\t");
         String usuario = scanner.next();//Pedir el valor por consola
         System.out.println("\nHola "+usuario);
+  */
+/*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proporciona el título:");
+        String titulo = scanner.nextLine();
+        System.out.println("Proporciona el autor:");
+        String autor = scanner.nextLine();
+        System.out.println(titulo +" fue escrito por "+autor);
+*/
+/*
+        //decimal
+        var b1 = 10;
+        System.out.println("b1 = " + b1);
+        //hexadecimal
+        var b2 = 0xa;
+        System.out.println("b2 = " + b2);
+        //octal
+        var b3 = 012;
+        System.out.println("b3 = " + b3);
+ */
+/*
+//byte, short, int, long
+        System.out.println("bits tipo byte:" + Byte.SIZE);
+        System.out.println("bytes tipo byte:" + Byte.BYTES);
+        System.out.println("valor minimo tipo byte:" + Byte.MIN_VALUE);
+        System.out.println("valor maximo tipo byte:" + Byte.MAX_VALUE);
+        System.out.println();
+        System.out.println("bits tipo short:" + Short.SIZE);
+        System.out.println("bytes tipo short:" + Short.BYTES);
+        System.out.println("valor minimo tipo short:" + Short.MIN_VALUE);
+        System.out.println("valor maximo tipo short:" + Short.MAX_VALUE);
+        System.out.println();
+        System.out.println("bits tipo int:" + Integer.SIZE);
+        System.out.println("bytes tipo int:" + Integer.BYTES);
+        System.out.println("valor minimo tipo int:" + Integer.MIN_VALUE);
+        System.out.println("valor maximo tipo int:" + Integer.MAX_VALUE);
+        System.out.println();
+        System.out.println("bits tipo long:" + Long.SIZE);
+        System.out.println("bytes tipo long:" + Long.BYTES);
+        System.out.println("valor minimo tipo long:" + Long.MIN_VALUE);
+        System.out.println("valor maximo tipo long:" + Long.MAX_VALUE);
+
+        byte byteVar = 15;
+        System.out.println("byteVar = " + byteVar);
+        short shortVar = 100;
+        System.out.println("shortVar = " + shortVar);
+        int intVar = 500;
+        System.out.println("intVar = " + intVar);
+        long longVar = 1000;
+        System.out.println("longVar = " + longVar);
+
+        var numero = 10;
+        System.out.println("numero = " + numero);
+ */
+/*
+    //float, double
+        System.out.println("bits tipo float:" + Float.SIZE);
+        System.out.println("bytes tipo float:" + Float.BYTES);
+        System.out.println("valor minimo tipo float:" + Float.MIN_VALUE);
+        System.out.println("valor maximo tipo float:" + Float.MAX_VALUE);
+        System.out.println();
+        System.out.println("bits tipo double:" + Double.SIZE);
+        System.out.println("bytes tipo double:" + Double.BYTES);
+        System.out.println("valor minimo tipo double:" + Double.MIN_VALUE);
+        System.out.println("valor maximo tipo double:" + Double.MAX_VALUE);
+
+        float floatVar = 1.05F;
+        double doubleVar = 1.0D;
+
+        var floatVar2 = 10.0F;
+        var doubleVar2 = 10.15D;
+ */
+        System.out.println("short1.SIZE = " + Short.MAX_VALUE);
+        System.out.println("short1.SIZE = " + Short.MIN_VALUE);
     }
 }
