0a400ab433aa5d9b5ff3631dc9c539d7a8a88264
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-07-07 19:25:17 -0500

    Metodos - Suma de numeros con constructor y con parametros

--- ProyectoAritmetica/src/Aritmetica.java
+++ ProyectoAritmetica/src/Aritmetica.java
@@ -1,8 +1,20 @@
 public class Aritmetica {
 
+    int a, b;
 
-    public int sumar(int a, int b) {
-        int suma = a + b;
-        return suma;
+    //Constructor sin argumentos
+    Aritmetica() {
     }
+
+    //Constructor con argumentos
+    Aritmetica(int arg1, int arg2) {
+        a = arg1;
+        b = arg2;
+    }
+
+    int sumar() {
+        return a + b;
+    }
+
 }
+
